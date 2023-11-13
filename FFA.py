5526df64481d6c6de8511f374112bd436789517a
ErickKikuchi <101877459+KaitoK1kuchi@users.noreply.github.com>
2023-11-12 21:49:50 -0300

    changed to == in die condition

--- FFA.py
+++ FFA.py
@@ -61,7 +61,7 @@ class FFA:
             evaluations += 1
             result_list = copy.deepcopy(self.ffs)
             result_list.sort(key=get_last_value)
-            if len(result_list) <= 0:
+            if len(result_list) == 0:
                 print("murieron todos")
                 break
             if get_last_value(result_list[0]) < 0.00001:
